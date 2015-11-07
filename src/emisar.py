Fixed omnibus to run in double precision

src/emisar.py
@@ -20,13 +20,13 @@ def read_emisar(fname):
     outDataset = driver.Create(outfn,cols,rows,9,GDT_Float32)  
     dtr = np.dtype(np.float32).newbyteorder('B')
     dtc = np.dtype(np.complex64).newbyteorder('B')
-    fn = path+'/'+root+'hhhh'
+    fn = path+'/'+root+'hhhh.emi'
     a = np.reshape(np.fromfile(fn,dtr),(rows,cols)) 
     a = np.rot90(a)
     outBand = outDataset.GetRasterBand(1)
     outBand.WriteArray(a.byteswap(),0,0) 
     outBand.FlushCache() 
-    fn = path+'/'+root+'hhhv'             
+    fn = path+'/'+root+'hhhv.emi'             
     a = np.reshape(np.fromfile(fn,dtc),(rows,cols)) 
     a = np.rot90(a)
     outBand = outDataset.GetRasterBand(2)
@@ -35,7 +35,7 @@ def read_emisar(fname):
     outBand = outDataset.GetRasterBand(3)
     outBand.WriteArray(np.imag(a).byteswap(),0,0)  
     outBand.FlushCache()          
-    fn = path+'/'+root+'hhvv' 
+    fn = path+'/'+root+'hhvv.emi' 
     a = np.reshape(np.fromfile(fn,dtc),(rows,cols)) 
     a = np.rot90(a)
     outBand = outDataset.GetRasterBand(4)
@@ -44,13 +44,13 @@ def read_emisar(fname):
     outBand = outDataset.GetRasterBand(5)
     outBand.WriteArray(np.imag(a).byteswap(),0,0) 
     outBand.FlushCache()  
-    fn = path+'/'+root+'hvhv'    
+    fn = path+'/'+root+'hvhv.emi'    
     a = np.reshape(np.fromfile(fn,dtr),(rows,cols)) 
     a = np.rot90(a)
     outBand = outDataset.GetRasterBand(6)
     outBand.WriteArray(a.byteswap(),0,0) 
     outBand.FlushCache() 
-    fn = path+'/'+root+'hvvv'  
+    fn = path+'/'+root+'hvvv.emi'  
     a = np.reshape(np.fromfile(fn,dtc),(rows,cols))
     a = np.rot90(a) 
     outBand = outDataset.GetRasterBand(7)
@@ -59,7 +59,7 @@ def read_emisar(fname):
     outBand = outDataset.GetRasterBand(8)
     outBand.WriteArray(np.imag(a).byteswap(),0,0)  
     outBand.FlushCache() 
-    fn = path+'/'+root+'vvvv' 
+    fn = path+'/'+root+'vvvv.emi' 
     a = np.reshape(np.fromfile(fn,dtr),(rows,cols)) 
     a = np.rot90(a)
     outBand = outDataset.GetRasterBand(9)
@@ -69,5 +69,5 @@ def read_emisar(fname):
     print 'emisar data writen to %s'%outfn
     
 if __name__=='__main__':
-    read_emisar('/home/mort/imagery/sar/emisar/fl074_l.hdr')
+    read_emisar('/home/mort/imagery/sar/emisar/fl062_l.hdr')
     

src/omnibus.py
@@ -21,57 +21,58 @@ import numpy as np
 from scipy import stats, ndimage
 import os, sys, time, getopt, gdal  
 from osgeo.gdalconst import GA_ReadOnly, GDT_Float32, GDT_Byte
+from mpmath import eps
 
-def getmat(fn,m,x0,y0,cols,rows,bands):
+def getmat(fn,x0,y0,cols,rows,bands):
 #  read 9- 4- or 1-band preprocessed files and return real/complex matrix elements 
     inDataset1 = gdal.Open(fn,GA_ReadOnly)     
     if bands == 9:
 #      T11 (k1)
         b = inDataset1.GetRasterBand(1)
-        k1 = m*b.ReadAsArray(x0,y0,cols,rows)
+        k1 = b.ReadAsArray(x0,y0,cols,rows)
 #      T12  (a1)
         b = inDataset1.GetRasterBand(2)
         a1 = b.ReadAsArray(x0,y0,cols,rows)
         b = inDataset1.GetRasterBand(3)    
         im = b.ReadAsArray(x0,y0,cols,rows)
-        a1 = m*(a1 + 1j*im)
+        a1 = (a1 + 1j*im)
 #      T13  (rho1)
         b = inDataset1.GetRasterBand(4)
         rho1 = b.ReadAsArray(x0,y0,cols,rows)
         b = inDataset1.GetRasterBand(5)
         im = b.ReadAsArray(x0,y0,cols,rows)
-        rho1 = m*(rho1 + 1j*im)      
+        rho1 = (rho1 + 1j*im)      
 #      T22 (xsi1)
         b = inDataset1.GetRasterBand(6)
-        xsi1 = m*b.ReadAsArray(x0,y0,cols,rows)    
+        xsi1 = b.ReadAsArray(x0,y0,cols,rows)    
 #      T23 (b1)        
         b = inDataset1.GetRasterBand(7)
         b1 = b.ReadAsArray(x0,y0,cols,rows)
         b = inDataset1.GetRasterBand(8)
         im = b.ReadAsArray(x0,y0,cols,rows)
-        b1 = m*(b1 + 1j*im)      
+        b1 = (b1 + 1j*im)      
 #      T33 (zeta1)
         b = inDataset1.GetRasterBand(9)
-        zeta1 = m*b.ReadAsArray(x0,y0,cols,rows) 
+        zeta1 = b.ReadAsArray(x0,y0,cols,rows) 
         return (k1,a1,rho1,xsi1,b1,zeta1)             
     elif bands == 4:
 #      C11 (k1)
         b = inDataset1.GetRasterBand(1)
-        k1 = m*b.ReadAsArray(x0,y0,cols,rows)
+        k1 = b.ReadAsArray(x0,y0,cols,rows)
 #      C12  (a1)
         b = inDataset1.GetRasterBand(2)
         a1 = b.ReadAsArray(x0,y0,cols,rows)
         b = inDataset1.GetRasterBand(3)
         im = b.ReadAsArray(x0,y0,cols,rows)
-        a1 = m*(a1 + 1j*im)        
+        a1 = (a1 + 1j*im)        
 #      C22 (xsi1)
         b = inDataset1.GetRasterBand(4)
-        xsi1 = m*b.ReadAsArray(x0,y0,cols,rows)  
+        xsi1 = b.ReadAsArray(x0,y0,cols,rows)  
         return (k1,a1,xsi1)         
     elif bands == 1:        
 #      C11 (k1)
         b = inDataset1.GetRasterBand(1)
-        k1 = m*b.ReadAsArray(x0,y0,cols,rows)  
+        k1 = b.ReadAsArray(x0,y0,cols,rows)  
         return k1
                        
 def main():
@@ -80,6 +81,7 @@ Usage:
 ------------------------------------------------
     python %s [-h] [-d dims] [-s significance] [-m] infile1,infile2,...,infilen outfn enl
                   (infiles are comma-separated, no blank spaces)
+                  outfn is without path (written to same directory as infile1)
     
     Perform change detection on multi-temporal, polarimetric SAR imagery.
 --------------------------------------------'''%sys.argv[0]
@@ -104,8 +106,8 @@ Usage:
         sys.exit(1)        
     fns = args[0].split(',')
     outfn = args[1]
-    m = float(eval(args[2])) # equivalent number of looks
-    n = float(len(fns))      # number of images
+    m = np.float64(eval(args[2])) # equivalent number of looks
+    n = np.float64(len(fns))      # number of images
     print '==============================================='
     print 'Multi-temporal Complex Wishart Change Detection'
     print '==============================================='
@@ -136,26 +138,35 @@ Usage:
     dirn = os.path.dirname(path)
     outfn = dirn + '/' + outfn 
     start = time.time()
-    sumlogdet = 0
-    k = 0; a = 0; rho = 0; xsi = 0; b = 0; zeta = 0
+    sumlogdet = 0.0
+    k = 0.0; a = 0.0; rho = 0.0; xsi = 0.0; b = 0.0; zeta = 0.0
     for fn in fns:
-        result = getmat(fn,m,x0,y0,cols,rows,bands)
+        result = getmat(fn,x0,y0,cols,rows,bands)
         if p==3:
             k1,a1,rho1,xsi1,b1,zeta1 = result
+            k1 = m*np.float64(k1)
+            a1 = m*np.complex128(a1)
+            rho1 = m*np.complex128(rho1)
+            xsi1 = m*np.float64(xsi1)
+            b1 = m*np.complex128(b1)
+            zeta1 = m*np.float64(zeta1)
             k += k1; a += a1; rho += rho1; xsi += xsi1; b += b1; zeta += zeta1 
             det1 = k1*xsi1*zeta1 + 2*np.real(a1*b1*np.conj(rho1)) - xsi1*(abs(rho1)**2) - k1*(abs(b1)**2) - zeta1*(abs(a1)**2)  
         elif p==2:
             k1,a1,xsi1 = result
+            k1 = m*np.float64(k1)
+            a1 = m*np.complex128(a1)
+            xsi1 = m*np.float64(xsi1)
             k += k1; a += a1; xsi += xsi1
             det1 = k1*xsi1 - abs(a1)**2
         elif p==1:
-            k1 = result
+            k1 = m*np.float64(result)
             k += k1
             det1 = k1
         x0 = 0 # subsequent files are warped to cols x rows
         y0 = 0           
         idx = np.where(det1 <= 0.0)
-        det1[idx] = 0.0001 
+        det1[idx] = 0.0000001 
         sumlogdet += np.log(det1) 
     if p==3: 
         detsum = k*xsi*zeta + 2*np.real(a*b*np.conj(rho)) - xsi*(abs(rho)**2) - k*(abs(b)**2) - zeta*(abs(a)**2) 
@@ -164,7 +175,7 @@ Usage:
     elif p==1:
         detsum = k  
     idx = np.where(detsum <= 0.0)
-    detsum[idx] = 0.0001 
+    detsum[idx] = 0.0000001 
     logdetsum = np.log(detsum)
     lnQ = m*(p*n*np.log(n) + sumlogdet - n*logdetsum)
     f =(n-1)*p**2
@@ -172,14 +183,14 @@ Usage:
     omega2 = p**2*(p**2 - 1)*(n/m**2 - 1/(m*n)**2)/(24*rho**2) - p**2*(n - 1)*(1 - 1/rho)**2/4
 #  test statistic    
     Z = -2*rho*lnQ
-#  change probabilty
+#  change probability
     P =  (1.-omega2)*stats.chi2.cdf(Z,[f])+omega2*stats.chi2.cdf(Z,[f+4])
     if medianfilter:
         P =  ndimage.filters.median_filter(P, size = (3,3))  # for noisy satellite data
 #  change map
     a255 = np.ones((rows,cols),dtype=np.byte)*255
     a0 = a255*0
-    c11 = np.log(k+0.0001) 
+    c11 = np.log(k+0.0000001) 
     min1 =np.min(c11)
     max1 = np.max(c11)
     c11 = (c11-min1)*255.0/(max1-min1)  
@@ -188,7 +199,8 @@ Usage:
     c11 = np.where(P>(1.0-significance),a0,c11)      
     cmap = np.where(P>(1.0-significance),a255,c11)
 #  write to file system        
-    driver = inDataset1.GetDriver()    
+#    driver = inDataset1.GetDriver()  
+    driver = gdal.GetDriverByName('ENVI')  
     outDataset = driver.Create(outfn,cols,rows,2,GDT_Float32)
     geotransform = inDataset2.GetGeoTransform()
     if geotransform is not None:
@@ -227,7 +239,16 @@ Usage:
     inDataset1 = None
     inDataset2 = None
     print 'elapsed time: '+str(time.time()-start) 
-    print 'change map image written to: %s'%outfn   
+    print 'change map image written to: %s'%outfn  
+# # test against Matlab   
+#     fn = '/home/mort/imagery/sar/emisar/m2rlnQ63646568'
+#     inDatasetx = gdal.Open(fn,GA_ReadOnly) 
+#     xb = inDatasetx.GetRasterBand(1)
+#     Zx = np.transpose(xb.ReadAsArray(0,0,cols,rows))
+#     print 'max %.10f'%np.max(Z-Zx)   
+#     print 'min %.10f'%np.min(Z-Zx)
+#     print 'mean(abs) %.10f'%np.mean(np.abs(Z-Zx))                            
+                                    
                 
 if __name__ == '__main__':
     main()     

src/wishart.py
@@ -28,6 +28,7 @@ def main():
 Usage:
 ------------------------------------------------
     python %s [-h] [-d dims] [-s significance] file1 file2 outfile enl1 enl2
+    outfile is without path (written to same directory as file1)
     
     Perform change detection on bitemporal, polarimetric SAR imagery.
 --------------------------------------------'''%sys.argv[0]
@@ -205,11 +206,11 @@ Usage:
         print 'Incorrect number of bands'
         return   
     idx = np.where(det1 <= 0.0)
-    det1[idx] = 0.0001   
+    det1[idx] = 0.0000001   
     idx = np.where(det2 <= 0.0)
-    det2[idx] = 0.0001 
+    det2[idx] = 0.0000001 
     idx = np.where(det3 <= 0.0)
-    det3[idx] = 0.0001  
+    det3[idx] = 0.0000001  
     lnQ = cst+m1*np.log(det1)+m2*np.log(det2)-(m2+m1)*np.log(det3)
 #  test statistic    
     Z = -2*rho*lnQ
@@ -219,7 +220,7 @@ Usage:
 #  change map
     a255 = np.ones((rows,cols),dtype=np.byte)*255
     a0 = a255*0
-    c11 = np.log(k1+0.0001) 
+    c11 = np.log(k1+0.0000001) 
     min1 =np.min(c11)
     max1 = np.max(c11)
     c11 = (c11-min1)*255.0/(max1-min1)  